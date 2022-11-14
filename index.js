779d5ee74292633846eade81a75acf97ab63dee4
Sodiq Farhan <sodiqfarhan360@gmail.com>
2022-11-14 17:16:17 +0100

    Update logic

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/api/shorturl', (req, res) => {
   dns.lookup(urlToShorten.url.includes('http://www.') ? urlToShorten.url.slice(7) : urlToShorten.url.slice(8), (err, addresses) => {
     if (err) return res.status(500).json({ 'error': err.message });
     const newUrl = {
-      "original_url": urlToShorten.url,
+      "original_url": new url (urlToShorten.url),
       // "short_url": urlToShorten.url.slice(12, 14) + addresses[addresses.length - 1],
       "short_url": Number(addresses.split('.').reverse()[0]),
       // addresses
